7217b84ebc77f6907658e223e03679d3ed28f97a
Henrique Santana <hsatana.sousass@gmail.com>
2024-02-26 05:07:37 -0300

    feat: model pessoa

--- src/model/Pessoa.groovy
+++ src/model/Pessoa.groovy
@@ -1,4 +1,16 @@
 package model
 
+import bd.Bd
+
 class Pessoa {
+    String nome, email, estado, cep, descricao
+    ArrayList <Competencia> competencias = []
+
+    def addCompetencia(Competencia competencia){
+        competencias.push(competencia)
+    }
+
+    def save() {
+        Bd.pessoas << this
+    }
 }
